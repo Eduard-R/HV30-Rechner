c84d3adc60873cf446434a037a1735b82100b3cf
Eduard-R <edi_r@gmx.de>
2020-05-01 19:21:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function HV30(diagonale1, diagonale2) {
+function HV30(diagonale1 = 0, diagonale2 = 0) {
     var result_d = ((diagonale1 + diagonale2) / 140) / 2;
 
     return 0.1891 * (294.2 / Math.pow(result_d, 2));
@@ -6,36 +6,34 @@ function HV30(diagonale1, diagonale2) {
 }
 
 function HVCalc() {
+    const diagInput1 = document.getElementById('diag1');
+    const diagInput2 = document.getElementById('diag2');
 
-    var diag1 = document.getElementById('diag1').value;
-    var diag2 = document.getElementById('diag2').value;
-    var result = document.getElementById('result-area');
+    appendTR(parseInt(diagInput1.value), parseInt(diagInput2.value));
+    
+    diagInput1.value = '';
+    diagInput2.value = '';
 
-    result.value = HV30(parseInt(diag1), parseInt(diag2));
-
-
+    // saveToLocalStorage();
 }
 
-function appendTR() {
-
+function appendTR(diag1 = 0, diag2 = 0) {
     // Neues Tabellenzeilen-Objekt erstellen
-    var newTR = document.createElement("tr");
-
-
+    const newTR = document.createElement("tr");
 
     // Neues Tabellenzellen-Objekt erstellen
-    var newTD = document.createElement("td");
+    const idTd = document.createElement("td");
+    const timeTd = document.createElement("td");
+    const valueTd = document.createElement("td");
 
-    // HTML einfügen
-    newTD.innerHTML = "tableData";
+    timeTd.innerHTML = moment().format('DD.MM.YYYY - HH:mm:ss');
+    valueTd.innerHTML = HV30(diag1, diag2);
+
     // Neue Tabellenzelle an Zeile "anhängen"
-    newTR.appendChild(newTD);
-    
-    
+    newTR.appendChild(idTd);
+    newTR.appendChild(timeTd);
+    newTR.appendChild(valueTd);
 
     // Neue Zeile in Tabelle "einhängen"
     document.getElementById("tableData").appendChild(newTR);
-    
 }
-
-
